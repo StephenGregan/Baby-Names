0cf581f922a65eb22a1de4847fe193c58962b7f6
StephenGregan <grego@live.ie>
2018-08-21 11:09:34 +0100

    update

--- babynamr/babynamr/app.js
+++ babynamr/babynamr/app.js
@@ -1,30 +1,37 @@
-﻿var showStats = true;
+﻿// If showStats is turned on, then the Status Controlled will be called.  The status controller check for an index, and if it doesn't find one it will create one.
+// If there are no names in the index, it will be populated with some.  This is great for getting started, but if you're up and running it will cause extra unneeded cycles.
+// It might come in handy.
+var showStats = true;
+
 var statusUri = 'api/status/Get';
 var searchUri = 'api/babyname/Get';
 
 $(document).ready(function () {
     if (showStats) {
         $.getJSON(statusUri)
-            .done(function (data) {
-                $('#statusBox').html("<h5>Status: " + data.StatusCode + " | # of Items: " + data.DocumentCount + " | Storage Size: " + data.storageSize + "</h5>");
-                $('#statusBox').addClass("alert alert-success");
+            .done(function () {
+                $('#statusBox').html();
+                $('#statusBix').addClass('alert alert-success');
             })
             .error(function (jqXHR, textStatus, errorThrown) {
                 $('#statusBox').html(errorThrown);
-                $('#statusBox').addClass("alert alert-danger");
+                $('#statusBox').addClass('alert alert-danger');
             });
     }
-    $('#txtSearch').keydown(function (event) {
+    // This is if the user presses enter on the search box.
+    $('#searchText').keydown(function (event) {
         if (event.keyCode == 13) {
-            search($('txtSearch').val());
+            search($('#txtSearch').val());
             return false;
         }
     });
 });
 
-function search() {
+// This is the big search Web API call.  All we do is pass the search terms to the search API, and sort htem by name.
+function search(name) {
     var isBoyChecked = $('#chkBoys').is(':checked');
-    var isGirlChecked = $('chkGirls').is(':checked');
+    var isGirlChecked = $('#chkGirls').is(':checked');
+
     var query = searchUri + '?q=' + name;
 
     if (isBoyChecked && isGirlChecked) {
@@ -36,24 +43,25 @@ function search() {
     if (isBoyChecked == false && isGirlChecked == true) {
         query = searchUri + '?q=' + name + '&gender=Female';
     }
+
     $.getJSON(query)
-        .done(function (data) {
+        .done(function data() {
             $('#dataName').empty();
-            $('#resultsFound').html("Results found: " + data.length);
+            $('#resultsFound').html("Results Found: " + data.length);
             for (var i = 0; i < data.length; i++) {
                 if (data[i].Document.gender == 'Male') {
-                    $('#dataName').append("<div class='row rowboy'><div class='col-md-12'><h3>" + data[i].Document.name + "<br /><small>" + data[i].Document.origin + "<br>" + data[i].Document.gender + "</br></small></h3>" + data[i].Document.meaning + "</div></div>");
+                    $('#dataName').append();
                 }
                 if (data[i].Document.gender == 'Female') {
-                    $('#dataName').append("div class='row rowgirl'><div class='col-md-12'><h3>" + data[i].Document.name + "<br /><small>" + data[i].Document.origin + "<br>" + data[i].Document.gender + "</br></small></h3>" + data[i].Document.meaning + "</div></div>");
+                    $('#dataName').append();
                 }
                 if (data[i].Document.gender == 'Both') {
-                    $('#dataName').append("<div class='row rowboth'><div class='col-md-12'><h3>" + data[i].Document.name + "<br /><small>" + data[i].Document.origin + "<br>" + data[i].Document.gender + "</br></small></h3>" + data[i].Document.meaning + "</div></div>");
+                    $('#dataName').append();
                 }
             }
-        })
-        .error(function (jqXHR, textStatus, errorThrown) {
-            $('resultsFound').html("Results Found: 0");
+        }).error(function (jqXHR, textStatus, errorThrown) {
+            $('#resultsFound').html("Results Found: 0");
+            // This is useful to check the search error.  It's not a great visual for the end user.
             $('#statusBox').html(errorThrown);
             $('#statusBox').addClass("alert alert-danger");
         });
